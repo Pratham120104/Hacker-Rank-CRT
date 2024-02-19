7c398a7257a29b602aa945e7d8dcbf8030a97958
Pratham120104 <145964506+Pratham120104@users.noreply.github.com>
2024-02-19 11:45:18 +0530

    Add files via upload

---
+++ HR1.c
@@ -0,0 +1,18 @@
+int compare(const void *a, const void *b) {
+    return (*(int*)a - *(int*)b);
+}
+
+int minimumAbsoluteDifference(int arr_count, int* arr) 
+{
+    qsort(arr, arr_count, sizeof(int), compare);
+    
+    int minAbsDiff = INT_MAX;
+    for (int i = 1; i < arr_count; i++) {
+        int currentAbsDiff = abs(arr[i] - arr[i - 1]);
+        if (currentAbsDiff < minAbsDiff) {
+            minAbsDiff = currentAbsDiff;
+        }
+    }
+
+    return minAbsDiff;
+}
